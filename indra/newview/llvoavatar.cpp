15acf0accb7851774c911fa238c6044e14bf7448
phr0z3nt04st <phr0z3nt04st@gmail.com>
2011-04-26 21:19:59 -0500

    Reset on turn_left and turn_right These do not play unless the user hits there A or D key on the keyboard, or a script plays them for them.

--- indra/newview/llvoavatar.cpp
+++ indra/newview/llvoavatar.cpp
@@ -5994,6 +5994,12 @@ BOOL LLVOAvatar::startMotion(const LLUUID& id, F32 time_offset)
 
 	LLMemType mt(LLMemType::MTYPE_AVATAR);
 
+	//since only the user can invoke these animations (scripts can explicitly) then reset the idle timer
+	if(id == ANIM_AGENT_TURNLEFT || id == ANIM_AGENT_TURNRIGHT)
+	{
+		mIdleTimer.reset();
+	}
+	
 	LLUUID remap_id = remapMotionID(id);
 
 	if (mIsSelf && remap_id == ANIM_AGENT_AWAY)
